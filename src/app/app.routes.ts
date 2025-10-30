eb451fc29b8599f35110c3f19d6f3e03c914da94
JefferGuti <jefferson.gutierrez.rojas@hotmail.com>
2025-10-29 20:00:18 -0500

    Configuración funcional

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: InicioComponent
+  }
+];
